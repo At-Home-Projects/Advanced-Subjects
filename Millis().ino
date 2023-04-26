fc9dd3d24cd2eea31218f3fa97aa79102e0f24d8
Comrade-Bacon <120524043+Comrade-Bacon@users.noreply.github.com>
2023-04-25 22:33:52 -0600

    Create Millis().ino

---
+++ Millis().ino
@@ -0,0 +1,12 @@
+/*
+* The millis() function is incredibly useful for timeing actions in your code.
+* what it does is when the arduino starts, the millis() function will start counting. (in milliseconds)
+* we can use this to time actions in our code. but also alowing us to do two things at once (tecnicly not but we will get into that later.)
+*
+
+void setup() {
+
+
+}
+
+void loop() {
