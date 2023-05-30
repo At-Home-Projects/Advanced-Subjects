0b5cd20cd4ba901e9a3fecc2751cb1123185f48e
Comrade-Bacon <120524043+Comrade-Bacon@users.noreply.github.com>
2023-05-30 00:16:14 -0600

    Update Serial Communication.ino

--- Serial Communication.ino
+++ Serial Communication.ino
@@ -1,7 +1,80 @@
 
 /*
+* The following is two sets of code. the first is the more convinent way, the second is the more basic, step by step way
+*/
+
+/*******************************************************************************/
+/*******************************************************************************/
+
+/*
+* Path 1 :
+*/
+
+
+// to use this program type commands into the serial monitor. try asking how its doing, or putting in a pin number to turn on/ off an LED
+
+const int maxMessageLength = 20;   // set the maximum length for the message                   
+
+void setup() {
+ Serial.begin(9600);               // setup the serial monitor
+ for (int i = 1; i < 14; i++) {    // for every pin on the uno (every PWM on Mega)
+   pinMode(i, OUTPUT);             // set the pin to output
+ }
+}
+
+void loop() {
+  if (Serial.available()) { // if there is somthing to be read in the serial monitor
+
+    String message = Serial.readStringUntil('\n'); // variable to get the string verstion of the message
+
+    int charInMessage = 0; // variable for how many chars are in the message
+
+    for (int i = 0; message[i] != '\0'; i++) { // for evry char in the message
+      charInMessage ++; // add one to the variable that holds the number of chars in the message
+    }
+
+    // turn the pin on/off and print the message
+    if (message.toInt()) {                    // if the message is a number
+      int num = message.toInt();              // make a variable to hold the number verstion of the message
+      if (num < 14 && num > 1) {              // if the number is a pin on the arduino board
+        if (digitalRead(num) == LOW) {        // if the pin is off
+          digitalWrite(num, HIGH);            // turn the pin on
+          Serial.println(message + " On");    // notify the user the pin is on
+        } else {                              // if the pin is om
+          digitalWrite(num, LOW);             // turn the pin off
+          Serial.println(message + " Off");   // notify the user the pin os off
+        }
+      } else {                                // if the message is not a pin number
+        Serial.println(message);              // print the message
+      }
+
+
+    } else if (charInMessage > maxMessageLength) { // if the message exeeds the max length
+      Serial.println("Message is too long!");      // notufy the user the message is too long
+    
+    } else {                     // if anything else
+      Serial.println(message);   // print the message
+    }
+
+    if (message == "How are you?") {            // if the message is this greeting
+      Serial.println("Good. How about you?");   // print 'Good. How about you?'
+    }
+    
+  }
+}
+
+/*******************************************************************************/
+/*******************************************************************************/
+
+/*
+* Path 2 :
+*/
+
+
+/*
 * in this code, we use the serial monior to send messages to the arduino. if the message is a string, we will pint the string.
-* if the given message is a number we will print the string and turn on an LED pluged into the pin with the number corrospondin to the given number.
+* if the given message is a number we will print the string and turn on an LED pluged into the pin with the number corrosponding to the given number.
+* if the pin is already on, we will turn it off.
 * if the message does not corospond to a pin, noting will happen exept the message being printed.
 * if the message is to long, we will print a message notifying the user.
 *
@@ -11,12 +84,13 @@
 */
 
 const int maxMessageLength = 20; // set the max amount of characters the message can be
+bool asking = false;             // varialbe for if the arduino is asking the user a question
 
 void setup() {
- Serial.begin(9600); // setup the serial monitor
+ Serial.begin(9600);             // setup the serial monitor
 
- for (int i = 1; i < 13; i++) { // for all pins on uno (all PWM pins on MEGA)
-    pinMode(i, OUTPUT);         // set pin to output
+ for (int i = 1; i < 13; i++) {  // for all pins on uno (all PWM pins on MEGA)
+    pinMode(i, OUTPUT);          // set pin to output
  }
 }
 
@@ -45,14 +119,24 @@ void loop() {
         
         message[messagePos] = '\0';            // Add null character to string (array)
         
-        if (atoi(message)) {       // if the given message is a number
-          int num = atoi(message); // make a variable to hold the message
-          if (num < 14 && (num > 1)) { // check that the given number is a pin
-            digitalWrite(num, HIGH); // set the the message equal to a pin, and set that pin to high
+        if (atoi(message)) {                   // if the given message is a number
+          
+          int num = atoi(message);             // make a variable to hold the number verstion of the message
+          if (num < 14 && (num > 1)) {         // check that the given number is a pin
+            
+            if (digitalRead(num) == LOW) {     // if the pin is already off
+              digitalWrite(num, HIGH);         // set the the message equal to a pin, and set that pin to high
+              Serial.println(String(message) + " On");  // tell the user the pin is on
+            } else {
+              digitalWrite(num, LOW);          // turn the LED in the given pin off
+              Serial.println(String(message) + " Off"); // tell the user the pin is off
+            }
+          } else {                             // if it is not a pin on the board
+            Serial.println(message);           // print the message
           }
+        } else {                               // if it is not a number
+          Serial.println(message);             // print the message
         }
-        
-      Serial.println(message); // Print the message
         
       } else {                                   // if the message is over the proper length
         Serial.readStringUntil('\n');            // read the string sent to the arduino until ther terminating char (new line)
