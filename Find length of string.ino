3f8e86a40d490379fae37ad4c11a75d5473f0538
Comrade-Bacon <120524043+Comrade-Bacon@users.noreply.github.com>
2023-06-08 23:13:11 -0600

    Update Find length of string.ino

--- Find length of string.ino
+++ Find length of string.ino
@@ -1,13 +1,7 @@
 
-
-/*
-Work in progress! has many bugs!
-*/
-
-
 // this program will show you how to find how many characters are in a string (the length) includeing spaces.
 
-const String str = "Hello, GitHub" // variable to hold our string
+const String str = "Hello, world"; // variable to hold our string
 
 void setup() {
   Serial.begin(9600); // setup serial monitor
@@ -17,9 +11,9 @@ void loop() {
  
   int charInMessage = 0; // variable to hold how long the string is
 
-  for (int i = 0; message[i] != '\0'; i++) {charInMessage ++;} // count every char, and save it to the varialble
+  for (int i = 0; str[i] != '\0'; i++) {charInMessage ++;} // count every char, and save it to the varialble
 
-  Serial.println(charInMessage) // print the number of char in this string
+  Serial.println(charInMessage); // print the number of char in this string
  
   while(true); // stop the program so it only runs once
 
